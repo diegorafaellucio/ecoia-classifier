7856ba667bc450b8d7aebd380e74e40aa8cca025
diego <diegorafaellucio@gmail.com>
2024-04-24 01:59:03 -0300

    filtro de mso em producao

--- apps/integrator/views.py
+++ apps/integrator/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from src.enum.classifier_api_return_messages_enum import ClassifierApiReturnMessagesEnum
+from src.handler.classifier_handler import ClassifierHandler
 
 # Create your views here.
+class ClassifierView(GenericAPIView):
+
+    # 2. Create
+    def post(self, request, *args, **kwargs):
+
+        classifier_suite = kwargs['classifier_suite']
+
+        ClassifierHandler.process_images(classifier_suite)
+
+        try:
+
+            return Response({"status": "success", "message": ClassifierApiReturnMessagesEnum.SUCCESS.value}, status=status.HTTP_200_OK)
+        except:
+            return Response({"status": "fail", "message": ClassifierApiReturnMessagesEnum.FAILURE.value}, status=status.HTTP_400_BAD_REQUEST)
--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,23 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt upgrade
+
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+
+sudo apt install python3.11 -y
 sudo apt install build-essential -y
 sudo apt install python3.11-dev -y
-
+sudo apt-get install libopenblas-dev -y
 sudo apt-get install python3-dev default-libmysqlclient-dev build-essential -y
 
-pip3.11 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu --break-system-packages --user
-pip3.11 install -r requeriments.txt --break-system-packages
+python3.11 get-pip.py
+
+pip3.11 install --upgrade setuptools
+pip3.11 install --upgrade setuptools-scm
+pip3.11 install --upgrade wheel
+
+pip3.11 install -r requeriments_base.txt --break-system-packages --no-cache-dir
+pip3.11 install -r requeriments_torch.txt --break-system-packages --no-cache-dir
+pip3.11 install -r requeriments_ultralytics.txt --break-system-packages --no-cache-dir
 
