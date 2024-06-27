cb29a283753d75701129d1fce9de87fab0bede13
diego <diegorafaellucio@gmail.com>
2024-06-27 08:41:08 -0300

    modelos da frigol aan

---
+++ src/enum/conformation_enum.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class ConformationEnum(Enum):
+    BB = 1
+    RR = 2
+    IN = 3
+    MB = 4
+    MANUFACTURA = 5
+    EE = 6
---
+++ src/utils/grease_color_utils.py
@@ -0,0 +1,37 @@
+import cv2
+from src.enum.grease_color_enum import GreaseColorEnum
+import numpy as np
+class GreaseColorUtils:
+
+
+
+    @staticmethod
+    def classify(detector, image):
+        image_features = GreaseColorUtils.get_histogram(image)
+
+        detection_results = detector.detect(image_features)
+        grease_color_model_id = str(detection_results[0])
+        grease_color_database_id = GreaseColorEnum.get_value(grease_color_model_id)
+
+        return grease_color_database_id
+
+    @staticmethod
+    def get_histogram(img, hsv=True):
+
+        if hsv:
+            img_analysy = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        else:
+            img_analysy = img.copy()
+
+        channels = cv2.split(img_analysy)
+        features = []
+        for c in channels:
+            hist = cv2.calcHist([c], [0], None, [256], [0, 256])
+            features.append(hist[1:])
+
+        features = np.hstack(features)
+        features = features.T
+        features = features.flatten()
+        return features.reshape((1, -1))
+
