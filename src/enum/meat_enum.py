a2c41da5669b69f461dccd8eea4294f66dede9e0
diego <diegorafaellucio@gmail.com>
2024-08-29 07:47:17 -0300

    update models

--- src/enum/meat_enum.py
+++ src/enum/meat_enum.py
@@ -9,3 +9,8 @@ class MeatEnum(Enum):
     UNIFORME = {'model_id':'4', 'database_id':'8'}
     EXCESSIVA = {'model_id':'5', 'database_id':'9'}
 
+    GRADO_0 = {'model_id': '1', 'database_id': '1'}
+    GRADO_1 = {'model_id': '2', 'database_id': '3'}
+    GRADO_2 = {'model_id': '3', 'database_id': '6'}
+    GRADO_3 = {'model_id': '4', 'database_id': '8'}
+
