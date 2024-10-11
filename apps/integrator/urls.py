15302636382fbcfa5b5bb5741cba8b98907f4dec
diego <diegorafaellucio@gmail.com>
2024-10-11 12:34:00 -0300

    docker support was add to project.

--- apps/integrator/urls.py
+++ apps/integrator/urls.py
@@ -1,19 +1,10 @@
 import logging
 from django.urls import path
-from apps.integrator.views.integrate_view import IntegrateView
-from apps.integrator.views.job_status_view import JobStatusView
-from src.job.integrator_job import IntegratorJob
-from src.enum.job_name_enum import JobNameEnum
-import threading
+from apps.integrator.views import IntegratorView
 
 logger = logging.getLogger(__name__)
 
 
-integrator_job_thread = threading.Thread(target=IntegratorJob.do, name=JobNameEnum.INTEGRATOR.value)
-integrator_job_thread.start()
-
-
 urlpatterns = [
-    path("integrate", IntegrateView.as_view()),
-    path("job_status", JobStatusView.as_view(), {JobNameEnum.INTEGRATOR.value: integrator_job_thread}),
+    path("integrator", IntegratorView.as_view()),
 ]
