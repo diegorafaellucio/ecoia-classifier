02fcfe1b9ee435640647b3f3cfc753740c8c844f
diego <diegorafaellucio@gmail.com>
2024-10-11 13:42:52 -0300

    code normalization

--- apps/integrator/urls.py
+++ apps/integrator/urls.py
@@ -1,10 +1,10 @@
 import logging
 from django.urls import path
-from apps.integrator.views import IntegratorView
+from apps.integrator.views.integrate_view import IntegrateView
 
 logger = logging.getLogger(__name__)
 
 
 urlpatterns = [
-    path("integrator", IntegratorView.as_view()),
+    path("integrator", IntegrateView.as_view()),
 ]
