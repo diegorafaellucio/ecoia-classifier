4c430f5aeb792b079eb2abfb024bb6217df61d22
diego <diegorafaellucio@gmail.com>
2024-10-28 14:28:28 -0300

    models add to git ignore

--- src/utils/classifier_utils.py
+++ src/utils/classifier_utils.py
@@ -38,59 +38,43 @@ class ClassifierUtils:
         classification_id = None
         filter_label = 'NAO_CLASSIFICADO'
         filter_confidence = 0.0
-        side_detection_result = None
 
-        skeleton_detection_result, intersection_score = ClassifierUtils.classify(skeleton_detector, image, get_intersection_score=True)
+        side_detection_result = ClassifierUtils.classify(side_detector, image)
 
-        if skeleton_detection_result is None:
-            classification_id = ClassificationErrorEnum.ERRO_93.value
+        if side_detection_result is None:
+            classification_id = ClassificationErrorEnum.ERRO_102.value
 
         else:
 
-            intersection_threshold =ConfigurationStorageController.get_config_data_value(
-                ConfigurationEnum.SKELETON_CLASSIFICATION_INTERSECTION_THRESHOLD.name)
+            filter_detection_result = ClassifierUtils.classify(filter_detector, image)
 
-            if intersection_score < intersection_threshold:
-                classification_id = ClassificationErrorEnum.ERRO_94.value
+            if filter_detection_result is None:
+                classification_id = ClassificationErrorEnum.ERRO_100.value
 
             else:
 
-                side_detection_result = ClassifierUtils.classify(side_detector, image)
+                filter_label = filter_detection_result['label']
+                filter_confidence = filter_detection_result['confidence']
 
-                if side_detection_result is None:
-                    classification_id = ClassificationErrorEnum.ERRO_102.value
+                filter_black_list = ConfigurationStorageController.get_config_data_value(
+                    ConfigurationEnum.FILTER_BLACK_LIST.name)
 
+                filter_in_black_List = ClassifierUtils.predicted_result_is_in_black_list(
+                    filter_detection_result, filter_black_list)
+
+                if filter_in_black_List:
+                    classification_id = ClassificationErrorEnum.ERRO_97.value
                 else:
 
-                    filter_detection_result = ClassifierUtils.classify(filter_detector, image)
+                    meat_detection_result = ClassifierUtils.classify(meat_detector, image)
 
-                    if filter_detection_result is None:
-                        classification_id = ClassificationErrorEnum.ERRO_100.value
-
+                    if meat_detection_result is None:
+                        classification_id = ClassificationErrorEnum.ERRO_101.value
                     else:
+                        meat_detection_label = meat_detection_result['label']
 
-                        filter_label = filter_detection_result['label']
-                        filter_confidence = filter_detection_result['confidence']
+                        meat_detataset_id = MeatEnum[meat_detection_label].value['database_id']
 
-                        filter_black_list = ConfigurationStorageController.get_config_data_value(
-                            ConfigurationEnum.FILTER_BLACK_LIST.name)
-
-                        filter_in_black_List = ClassifierUtils.predicted_result_is_in_black_list(
-                            filter_detection_result, filter_black_list)
-
-                        if filter_in_black_List:
-                            classification_id = ClassificationErrorEnum.ERRO_97.value
-                        else:
-
-                            meat_detection_result = ClassifierUtils.classify(meat_detector, image)
-
-                            if meat_detection_result is None:
-                                classification_id = ClassificationErrorEnum.ERRO_101.value
-                            else:
-                                meat_detection_label = meat_detection_result['label']
-
-                                meat_detataset_id = MeatEnum[meat_detection_label].value['database_id']
-
-                                classification_id = meat_detataset_id
+                        classification_id = meat_detataset_id
 
         return classification_id, filter_label, filter_confidence, side_detection_result
