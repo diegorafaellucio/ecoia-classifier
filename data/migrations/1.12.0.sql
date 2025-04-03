40eee410acf04d956b485bc486805ddf4e0c4d33
Iberns <i.bernardes@ecotrace.info>
2025-04-03 16:02:26 -0300

    feature de mapeamento das lesoes

--- data/migrations/1.12.0.sql
+++ data/migrations/1.12.0.sql
@@ -1,11 +1,8 @@
-alter table image
-    add carcass_detection_confidence_score decimal(10, 2) null after filter_confidence;
+alter table image add carcass_detection_confidence_score decimal(10, 2) null after filter_confidence;
 
-alter table image
-    add carcass_intersection_score decimal(10, 2) null after filter_confidence;
+alter table image add carcass_intersection_score decimal(10, 2) null after filter_confidence;
 
 
-alter table image
-    add carcass_classification_score decimal(10, 2) null after filter_confidence;
+alter table image add carcass_classification_score decimal(10, 2) null after filter_confidence;
 
 update aux_module set current_version='1.12.0', created_at=now(), updated_at=now() where name='ecoia-classifier'
