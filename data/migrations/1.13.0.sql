76fec89b8b3d4972103ee8f8b192e898f828a234
Iberns <i.bernardes@ecotrace.info>
2025-04-03 16:07:50 -0300

    migrations 1.13.0

--- data/migrations/1.13.0.sql
+++ data/migrations/1.13.0.sql
@@ -1,2 +1,2 @@
 ALTER TABLE bruise add COLUMN region_bruise_code VARCHAR(10) not null  DEFAULT 'D1';
-
+update aux_module set current_version='1.13.0', updated_at=now() where name='ecoia-classifier'
